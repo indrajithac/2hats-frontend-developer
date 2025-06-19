1dc772ca1a8e046035b46fda65dc5e8d0de645b3
indrajithac <indrajithac@gmail.com>
2025-06-19 09:16:46 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,50 @@
+import logoWhite from "../../assets/logoWhite.svg";
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <footer className="bg-dark text-white text-center py-3">
-      <p className="m-0">&copy; 2025 2Hats E-Commerce Test</p>
+    <footer className={styles.containerBlackPadding}>
+      <div className="container">
+        <div className={styles.footerDiv}>
+          <div className={styles.footerContent}>
+            <img
+              src={logoWhite}
+              alt="Logo"
+              className="w-[120px] h-[34px] sm:w-[220px] sm:h-[44px] object-contain"
+            />
+            <div className={styles.footerContentSection2}>
+              <div>
+                <p>Products</p>
+                <p className={styles.footerContentSectionText}>Sand Stone</p>
+                <p className={styles.footerContentSectionText}>Stone</p>
+                <p className={styles.footerContentSectionText}>Cement</p>
+                <p className={styles.footerContentSectionText}>Soft Stone</p>
+              </div>
+
+              <div>
+                <p>Services</p>
+                <p className={styles.footerContentSectionText}>
+                  Measurement Service
+                </p>
+                <p className={styles.footerContentSectionText}>
+                  Product Advice
+                </p>
+                <p className={styles.footerContentSectionText}>
+                  Interior Design
+                </p>
+              </div>
+
+              <div className={styles.twoSpan}>
+                <p>Contact information</p>
+                <p className={styles.footerContentSectionText}>
+                  3181 Al Imam Saud Ibn Abdul Aziz Branch Rd, An Nuzhah, Riyadh
+                  12474, Saudi Arabia
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
